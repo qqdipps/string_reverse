ceb39cb111550959fd02bc218da00389bded2f79
shrutivanw <shruti.dalvi@gmail.com>
2017-12-19 12:49:53 -0800

    Defining test structure for CS Fundamentals homework
    Defining test structure for CS Fundamentals spec style tests. The same structure will be copied to solution's branch and followed for other homework repos.

--- specs/string_reverse_spec.rb
+++ specs/string_reverse_spec.rb
@@ -2,53 +2,71 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/string_reverse'
 
-describe "basic test 1 - string reverse" do
-  it "reverse a string with odd number of characters" do
-    test_string = "Hello"
-    string_reverse(test_string)
-    test_string.must_equal "olleH"
-  end
-end
+describe "string reverse" do
+  describe "basic tests" do
+    describe "test 1" do
+      it "reverse a string with odd number of characters" do
+        test_string = "Hello"
 
-describe "basic test 2 - string reverse" do
-  it "reverse a string with even number of characters" do
-    test_string = "Software"
-    string_reverse(test_string)
-    test_string.must_equal "erawtfoS"
-  end
-end
+        string_reverse(test_string)
 
-# if it's a string parameter, check for empty
-describe "empty string - string reverse" do
-  it "reverse an empty string" do
-    test_string = ""
-    string_reverse(test_string)
-    test_string.must_be_empty
-  end
-end
+        test_string.must_equal "olleH"
+      end
+    end
 
-# if the parameter is an object, check for nil
-describe "nil - string reverse" do
-  it "nil object passed to reverse" do
-    test_string = nil
-    string_reverse(test_string)
-    test_string.must_be_nil
-  end
-end
+    describe "test 2" do
+      it "reverse a string with even number of characters" do
+        test_string = "Software"
 
-# check for edge cases
-describe "edge case 1 - string reverse" do
-  it "string with one character passed to reverse" do
-    test_string = "?"
-    string_reverse(test_string)
-    test_string.must_equal "?"
-  end
-end
+        string_reverse(test_string)
 
-describe "edge case 2 - string reverse" do
-  it "a really long string passed to reverse" do
-    test_string = "Oh, Supercalifragelisticexpialidocious! Even though the sound of it is something quite atrocious. If you say it loud enough, you'll always sound precocious. Supercalifragelisticexpialidocious!"
-    string_reverse(test_string)
-    test_string.must_equal "!suoicodilaipxecitsilegarfilacrepuS .suoicocerp dnuos syawla ll'uoy ,hguone duol ti yas uoy fI .suoicorta etiuq gnihtemos si ti fo dnuos eht hguoht nevE !suoicodilaipxecitsilegarfilacrepuS ,hO"
+        test_string.must_equal "erawtfoS"
+      end
+    end
+  end
+
+  # if it's a string parameter, check for empty
+  describe "empty string input test" do
+    it "reverse an empty string" do
+      test_string = ""
+
+      string_reverse(test_string)
+
+      test_string.must_be_empty
+    end
+  end
+
+  # if the parameter is an object, check for nil
+  describe "nil object input test" do
+    it "nil object passed to reverse" do
+      test_string = nil
+
+      string_reverse(test_string)
+
+      test_string.must_be_nil
+    end
+  end
+
+  # check for edge cases
+  describe "edge cases"
+    describe "edge case 1" do
+      it "string with one character passed to reverse" do
+        test_string = "?"
+
+        string_reverse(test_string)
+
+        test_string.must_equal "?"
+      end
+    end
+
+    describe "edge case 2" do
+      it "a really long string passed to reverse" do
+        test_string = "Oh, Supercalifragelisticexpialidocious! Even though the sound of it is something quite atrocious. If you say it loud enough, you'll always sound precocious. Supercalifragelisticexpialidocious!"
+
+        string_reverse(test_string)
+
+        test_string.must_equal "!suoicodilaipxecitsilegarfilacrepuS .suoicocerp dnuos syawla ll'uoy ,hguone duol ti yas uoy fI .suoicorta etiuq gnihtemos si ti fo dnuos eht hguoht nevE !suoicodilaipxecitsilegarfilacrepuS ,hO"
+      end
+    end
   end
 end
