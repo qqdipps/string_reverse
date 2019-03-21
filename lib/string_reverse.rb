efc280d4a4b161328772f57414ca30310455c53a
qqdipps <sdipp007@gmail.com>
2019-03-20 22:27:40 -0700

    green to go

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,11 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  return if !my_string
+  (my_string.length / 2).times do |i|
+    temp = my_string[i]
+    my_string[i] = my_string[-i - 1]
+    my_string[-i - 1] = temp
+  end
+  return my_string
 end
