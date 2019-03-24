36672337c707d89891c801c22dcbb03089987ba3
qqdipps <sdipp007@gmail.com>
2019-03-23 17:13:53 -0700

    added explanation of complex. analysis.

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,6 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the number of elems in string.
+# Space complexity: O(1)
 def string_reverse(my_string)
   return if !my_string
   (my_string.length / 2).times do |i|
